bb670d8983562c9f2bd8e56072544d5f69c781c1
Michael bejarano <michaelbejarano940@gmail.com>
2025-08-25 20:35:23 -0500

    pipi

--- src/StudentController.java
+++ src/StudentController.java
@@ -2,5 +2,6 @@ public class StudentController{
     public void controller(){
         //cambio mi primer hola mundo compartineod con otro programador
         println("hola mudno")
+        //pipipi
     }
 }
